667ffc70249fee5677fa9dc6e57eeb3ff3b95c5f
Edward Dume <edwarddume55@gmail.com>
2023-11-12 13:45:55 +0300

    adding user login/authauntication function page

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from .models import Room,Topic
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
 from .forms import RoomForm
 from django.db.models import Q
 # Create your views here.
@@ -9,6 +12,29 @@ from django.db.models import Q
 #      {'id':2, 'name':'Design with me'},
 #       {'id':3, 'name':'Frontend developers'},
 # ]
+
+def loginPage(request):
+    if request.method=='POST':
+        username= request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user=User.objects.get(username=username)
+        except:
+            messages.error(request,'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        
+        else:
+            messages.error(request,'Username OR password does not exist')
+
+
+    context= {}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q=request.GET.get('q') if request.GET.get('q') != None else ''
